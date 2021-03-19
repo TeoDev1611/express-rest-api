dfe01773db773641a1b1e19b3b9bcb615993b08b
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-03-19 18:23:18 -0500

    feat: change to dayjs

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,11 +1,11 @@
-const moment = require("moment");
+const dayjs = require('dayjs')
 
 // Middlewars
 const logger = (req, res, next) => {
   console.log(
     `${req.protocol}://${req.get("host")}${
       req.originalUrl
-    }: ${moment().format()}`
+    }: ${dayjs().format()}`
   );
   next();
 };
